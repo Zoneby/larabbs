d01db8b1025591e239bf2bb3b4db53a7f1487a81
5264 <zhusong@37.com>
2018-10-26 15:20:23 +0800

    #larabbs#WYSIWYG Editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
 	<title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
 
 	<link rel="stylesheet" href="{{asset('css/app.css')}}">
+	@yield('styles')
 </head>
 <body>
 	<div id="app" class="{{ route_class()}}-page">
@@ -22,5 +23,6 @@
 	</div>
 
 	<script src="{{asset('js/app.js')}}"></script>
+	@yield('scripts')
 </body>
 </html>
