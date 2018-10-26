e268ed3e98c6854c580accf7f74b19e984ba74b6
5264 <zhusong@37.com>
2018-10-26 10:36:25 +0800

    #larabbs#Topic order

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -9,9 +9,15 @@ use App\Models\Category;
 class CategoriesController extends Controller
 {
     //
-    public function show(Category $category)
+    public function show(Category $category, Request $request, Topic $topic)
     {
-    	$topics = Topic::where('category_id', $category->id)->paginate(20);
+    	// $topics = Topic::where('category_id', $category->id)->paginate(20);
+    	$topics = $topic->withOrder($request->order)
+    					->where('category_id', $category->id)
+    					->paginate(20);
+    	
+
+
 
     	// 传参变量话题和分类到模版中
     	return view('topics.index', compact('topics', 'category'));
